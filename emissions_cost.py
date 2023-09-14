a44f80fd9d86fab8a5207d95487ac98157b189db
ArnavSankhe <76445827+ArnavSankhe@users.noreply.github.com>
2023-09-14 10:56:31 +0100

    adding test

--- emissions_cost.py
+++ emissions_cost.py
@@ -1,31 +1,49 @@
 
-def get_emissions(distance,mode,journeytype): # all conversion values taken from google for the year 2022-23 
-    if mode == 'Plane':
-        co2 = distance * 0.194028
-    elif mode =='Tube':
-        co2 = distance * 0.02781
-    elif mode == 'Metro':
-        co2 = distance * 0.02813
-    elif mode == 'Tram':
-        co2 = distance * 0.02813
-    elif mode == 'Bus':
-        co2 = distance * 0.081008
-    elif mode == 'Train':
-        co2 = distance * 0.03549
-    elif mode == 'Taxi':
-        co2 = distance * 0.17646
-    elif mode == 'Motorbike':
-        co2 = distance * 0.10749
-    else:
-        co2 = distance * 0.144996
+def get_emissions(distance, mode, journeytype):
+    """
+    Calculates the amount of CO2 emissions based on the distance traveled, the mode of transportation, and the type of journey.
 
+    Args:
+        distance (float): The distance traveled in kilometers.
+        mode (string): The mode of transportation. Possible values are 'Plane', 'Tube', 'Metro', 'Tram', 'Bus', 'Train', 'Taxi', and 'Motorbike'.
+        journeytype (string): The type of journey. Possible values are 'Return' or any other value.
+
+    Returns:
+        float: The amount of CO2 emissions in metric tons.
+    """
     if journeytype == 'Return':
-        return float((co2*2))
-    else:
-        return float(co2)
+        distance *= 2
+
+    emission_factors = {
+        'Plane': 0.194028,
+        'Tube': 0.02781,
+        'Metro': 0.02813,
+        'Tram': 0.02813,
+        'Bus': 0.081008,
+        'Train': 0.03549,
+        'Taxi': 0.17646,
+        'Motorbike': 0.10749
+    }
+
+    co2 = distance * emission_factors.get(mode, 0.144996)
+
+    return float(co2)
 
 
 def cost(emissions):
-    """Computes cost from emissions. This is based on the cost of offsetting per tCO2e from
-    ClimateCare."""
+    """
+    Computes the cost of offsetting emissions based on the amount of emissions in tCO2e.
+
+    # existing code for the 'cost' function
+    pass
+    Parameters:
+    emissions (float): The amount of emissions in tCO2e.
+
+    Returns:
+    float: The cost of offsetting the emissions.
+
+    Example:
+    >>> cost(5000)
+    42.5
+    """
     return round(emissions / 1000 * 8.5, 2)
